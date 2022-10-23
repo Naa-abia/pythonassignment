39c84d8939024ccbde6ae966b7052227c876a686
Naa-abia <augusta.addy@meltwater.org>
2022-10-23 13:46:24 +0000

    while loop

--- even_numbers.py
+++ even_numbers.py
@@ -3,4 +3,9 @@ for current_number in range(1,30):
         print(current_number)
 
 
-        
+
+current_number = 1
+while current_number < 30 :
+    if current_number % 2 == 0 :
+        print(current_number)
+    current_number += 1 
